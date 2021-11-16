a23c31fa83301e195924d07248b2c15c0d7e5f3f
email <winiar.kontakt@gmail.com>
2021-11-16 12:23:59 +0100

    argument test - Filed properly

--- faktoryzacja/testy/prime_testy.py
+++ faktoryzacja/testy/prime_testy.py
@@ -7,11 +7,18 @@ def test_import_prime_factors():
         from prime import prime_factors  
     except ImportError as error:  
         assert False, error
-
+        
+def test_intiger_argument():
+    try:
+        from prime import prime_factors
+        prime_factors(123)  
+    except TypeError as error:  
+        assert False, error
 
 if __name__ == '__main__':
     for test in (
         test_import_prime_factors,
+        test_intiger_argument,
     ):  
         print(f'{test.__name__}: ', end='')
         try:
