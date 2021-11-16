d80a32a26a97d040230892ebe79e70b82b137595
email <winiar.kontakt@gmail.com>
2021-11-16 14:35:35 +0100

    return list test

--- faktoryzacja/testy/prime_testy.py
+++ faktoryzacja/testy/prime_testy.py
@@ -15,10 +15,21 @@ def test_intiger_argument():
     except TypeError as error:  
         assert False, error
 
+def test_return_list():
+    from prime import prime_factors
+    result = prime_factors(123)  
+    assert isinstance(result,list), "Filed - result is not list"
+
+def test_even_number_divisible():
+    from prime import prime_factors
+    result = prime_factors(10)  
+    assert isinstance(result,list), "Filed - result is not list"
+
 if __name__ == '__main__':
     for test in (
         test_import_prime_factors,
         test_intiger_argument,
+        test_return_list,
     ):  
         print(f'{test.__name__}: ', end='')
         try:
