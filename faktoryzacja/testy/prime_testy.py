6d4f7814d669c87d7f4c7cbc4c673694215960c9
email <winiar.kontakt@gmail.com>
2021-11-16 15:01:30 +0100

    add test_odd_case_factor

--- faktoryzacja/testy/prime_testy.py
+++ faktoryzacja/testy/prime_testy.py
@@ -25,12 +25,19 @@ def test_even_number_divisible():
     result = prime_factors(40)  
     assert result == [2,2,2], "Filed - even number dvisible error"
 
+def test_odd_case_factor():
+    from prime import prime_factors
+    result = prime_factors(40)  
+    assert result == [2,2,2,5], "Filed - odd number dvisible error"
+
+
 if __name__ == '__main__':
     for test in (
         test_import_prime_factors,
         test_intiger_argument,
         test_return_list,
         test_even_number_divisible,
+        test_odd_case_factor,
     ):  
         print(f'{test.__name__}: ', end='')
         try:
