dbcd470daf9d8ee4fa64a8566d485eb6c438c8ac
email <winiar.kontakt@gmail.com>
2021-11-16 16:09:33 +0100

    pass test_odd_case_factor

--- faktoryzacja/prime.py
+++ faktoryzacja/prime.py
@@ -1,7 +1,19 @@
+import math
 def prime_factors(number: int) -> list:
     factors = []
     #even number divisible
     while number % 2 == 0:
         factors.append(2)
         number = number / 2
+    
+    #n became odd
+    for i in range(3,int(math.sqrt(number))+1,2):
+     
+      while (number % i == 0):
+        factors.append(i)
+        number = number / i
+    
+    if number > 2:
+        factors.append(number)
+        
     return factors
