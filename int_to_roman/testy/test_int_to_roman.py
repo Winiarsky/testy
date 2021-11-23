4854a56ab6e7d183876df076f328dc5b0b3cf3a3
email <winiar.kontakt@gmail.com>
2021-11-23 23:10:14 +0100

    test only intiger argument

--- int_to_roman/testy/test_int_to_roman.py
+++ int_to_roman/testy/test_int_to_roman.py
@@ -1,4 +1,8 @@
 from int_to_roman import int_to_Roman
-
+import pytest
 def test_int_argument():
     int_to_Roman(1)
+
+def test_only_int_argument():
+    with pytest.raises(AttributeError):
+        int_to_Roman("123") 
